1d00335e054d16002b7c9e0c8d48f0395efb7a4a
Ciervos <mlujafo@gmail.com>
2022-01-02 18:58:45 -0300

    proceso parte juego

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -3,6 +3,7 @@ import React,{useState,useEffect} from 'react';
 import { StyleSheet, Text, View,TextInput,Image,Button} from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import ItemList from '../components/Lists/itemList';
+import { Ionicons } from '@expo/vector-icons';
 import colors from '../constants/colors';
 
 
@@ -10,12 +11,14 @@ const GameScreen = props => {
   const dispatch = useDispatch();
   const mobs = useSelector(state => state.enemies.enemies);
   const ourItems = useSelector(state => state.items.items);
+  const currLife = useSelector(state => state.character.life);
   const [currMob,setCurrMob] = useState(null)
   const [currItems,setCurrItems] = useState(ourItems)
+  const [fullLife,setFullLife] = useState(true)
 
   useEffect(() => {
     const random = Math.floor(Math.random() * mobs.length)
-    console.log(currItems)
+    
     checkItems(ourItems)
     setCurrMob(mobs[random]) 
 }, []);
@@ -24,7 +27,7 @@ const checkItems = (items) =>{
   
   let newArray = []
    items.map(function(obj){
-    console.log(newArray)
+    
     
     if(obj.selected){
      newArray.push(obj)
@@ -43,6 +46,7 @@ const handleSelected = () =>{
 
   return (
     <View style={styles.container}>
+      <View style={styles.subcontainer}>
       <Text style={styles.guide}> Elige un item para avanzar</Text>
    
                 {currMob
@@ -55,7 +59,12 @@ const handleSelected = () =>{
        {currMob
           ?<Text style={styles.desc}>{currMob.description}</Text>: null}
 
-       <ItemList inventory={currItems} handleSelected={handleSelected}/>   
+       <ItemList inventory={currItems} handleSelected={handleSelected}/>  
+       <View style={styles.life}>
+       <Text style={styles.guide}>Vida: </Text>
+       {fullLife ? <Ionicons name="heart-sharp" size={30} color={'red'} />:<Ionicons name="heart-half" size={30} color={'red'} />} 
+       </View>
+      </View>
     </View>
     
   );
@@ -68,6 +77,14 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        
+      },
+      subcontainer:{
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 18,
       },
       guide:{
         fontFamily: 'PressStart2P',
@@ -80,6 +97,11 @@ const styles = StyleSheet.create({
        desc:{
         fontFamily: 'PressStart2P',
         fontSize: 10
+       },
+       life:{
+       height: 50,
+       flexDirection: 'row',
+       margin: 20
        }
   
 });
